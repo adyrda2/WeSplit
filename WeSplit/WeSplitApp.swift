43f333a5920e4280a4880c5849cb79b85e6bea1c
Angela Dyrda <adyrda2@gmail.com>
2023-08-08 18:14:30 -0500

    Add WeSplit project

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Angela Dyrda on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
